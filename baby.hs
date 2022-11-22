fd40c4aa3510ff9d772498e6f55ef29134cb91a0
ShijieQin2021 <qsfshx@163.com>
2022-11-22 14:27:15 +0000

    first commit

---
+++ baby.hs
@@ -0,0 +1,17 @@
+doubleMe x = x + x
+
+doubleUs x y = x*2 + y*2
+
+doubleUs2 x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+			then x
+			else x*2
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1  
+
+conanO'Brien  = "hello"
+
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!"| x <- xs, odd x]
+
+removeUppercases xs = [x | x <- xs, not (x `elem` ['A'..'Z'])]
